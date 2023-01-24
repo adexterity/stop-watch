01c98187ac18b95ac48a9abd35a82453fe9d88e4
joseph suberu <opensesame4me@gmail.com>
2023-01-24 14:55:47 +0100

    an updated version of the app

--- app.js
+++ app.js
@@ -7,45 +7,50 @@ const resetBtn = document.querySelector(".btn-reset");
 let setTimer = null;
 let working = true;
 
-//reset default function 
-function resetDefault(){
-  
-  seconds.value = 0;
+//reset default function
+function resetDefault() {
+  seconds.value = "00";
 
-  minutes.value = 0;
+  minutes.value = "00";
 
   container.style.backgroundColor = "rgba(255, 255, 255, 0.158)";
   clearTimer();
   working = true;
+  seconds.removeAttribute("disabled");
+  minutes.removeAttribute("disabled");
 }
 
-
-//reset button 
+//reset button
 resetBtn.addEventListener("click", () => {
   resetDefault();
+  startBtn.textContent = "start";
 });
-
 
 //start button
 startBtn.addEventListener("click", () => {
-  if(working){
+  if (working) {
+    seconds.setAttribute("disabled", "");
+    minutes.setAttribute("disabled", "");
     working = false;
-  if(minutes.value!= 0 || seconds.value != 0){
-  function startTime() {
-    setTimer = setInterval(timer, 1000);
-  }
-  startTime();
-  }
-  
+    if (minutes.value != 0 || seconds.value != 0) {
+      startBtn.innerHTML = "pause &#9208;";
+      function startTime() {
+        setTimer = setInterval(timer, 1000);
+      }
+      startTime();
+    }
+  } else {
+    // startBtn.textContent = 'stop';
+    working = true;
+    clearTimer();
+    startBtn.innerHTML = "start &#9654;";
   }
 });
 
-
-//timer function 
+//timer function
 function timer() {
   if (seconds.value == 0 && minutes.value == 0) {
-    resetDefault()
-   
+    resetDefault();
   } else if (seconds.value != 0) {
     if (minutes.value == 0 && seconds.value <= 5) {
       container.style.backgroundColor = "rgba(255, 0, 0, 0.7)";
@@ -54,10 +59,11 @@ function timer() {
     seconds.value--;
   } else if (minutes.value != 0 && seconds.value == 0) {
     seconds.value = 59;
-    minutes.value -= 1;
+    minutes.value--;
   }
 }
 
+//this function clears the interval timer
 function clearTimer() {
   clearInterval(setTimer);
 }
